22e6543c4b7414111c14388022b14e56c959dc3f
Adan Chavez OLivera <adanch22@gmail.com>
2023-09-14 23:51:45 -0600

    pages components

---
+++ src/app/shared/brandcrumbs/brandcrumbs.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-brandcrumbs',
+  templateUrl: './brandcrumbs.component.html',
+  styles: [
+  ]
+})
+export class BrandcrumbsComponent {
+
+}
